c1bc8f84d9234198d50828e4a511e2a32f8e31aa
Ikaika <iDiffusion@users.noreply.github.com>
2018-01-14 13:13:40 -0500

    Add files via upload

--- src/Point.java
+++ src/Point.java
@@ -1,145 +1,67 @@
-import javax.vecmath.Vector3d;
-/**
- * A point object holds x, y, z values as well as r, g, b values.
- * 
- * @author Keegan Bruer <br>
- */
-public class Point implements Comparable<Point> {
-	double x, y, z;
-	int r, g, b;
-	/**
-	 * create point with x, y, z and black as the color.
-	 * @param x
-	 * @param y
-	 * @param z
-	 */
-	public Point(double x, double y, double z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		r = 0;
-		g = 0;
-		b = 0;
-	}
-	/**
-	 * create new point specifying the x, y, z and r, g, b values.
-	 * @param x
-	 * @param y
-	 * @param z
-	 * @param r
-	 * @param g
-	 * @param b
-	 */
-	public Point(double x, double y, double z, int r, int g, int b) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
-	
-	/**
-	 * set XYZ after being created.
-	 * @param x
-	 * @param y
-	 * @param z
-	 */
-	public void setXYZ(double x, double y, double z) {
-		this.x = x;
-		this.y = y;
-		this.z = z;
-	}
-	
-	/**
-	 * Get the xyz values as a array of doubles.
-	 * @return
-	 */
-	public double[] getXYZAsArray() {
-		double[] rtn = {x, y, z};
-		return rtn;
-	}
-	
-	/**
-	 * Returns XYZ as Vector3f
-	 * @return - Vector3f of XYZ
-	 */
-	public Vector3d getXYZ() {
-		return new Vector3d(x, y, z);
-	}
-	
-	/**
-	 * set the RGB values after being created.
-	 * @param r
-	 * @param g
-	 * @param b
-	 */
-	public void setRGB(int r, int g, int b) {
-		this.r = r;
-		this.g = g;
-		this.b = b;
-	}
-	/**
-	 * get array of RGB values as doubles.
-	 * @return
-	 */
-	public int[] getRGBAsArray() {
-		int[] rtn = {r, g, b};
-		return rtn;
-	}
-	
-	public Vector3d getRGB() {
-		return new Vector3d(r, g, b);
-	}
-	
-	/**
-	 * set x position
-	 * @param x
-	 */
-	public void setX(double x) {
-		this.x = x;
-	}
-	/**
-	 * set y position
-	 * @param y
-	 */
-	public void setY(double y) {
-		this.y = y;
-	}
-	/**
-	 * set z position
-	 * @param z
-	 */
-	public void setZ(double z) {
-		this.z = z;
-	}
-	/**
-	 * get x position
-	 * 
-	 */
-	public double getX() {
-		return this.x;
-	}
-	/**
-	 * get y position
-	 * 
-	 */
-	public double getY() {
-		return this.y;
-	}
-	/**
-	 * get z position
-	 * 
-	 */
-	public double getZ() {
-		return this.z;
-	}
+import javax.vecmath.Vector3f;
 
-	/**
-	 * used to sort Points in an ArrayList
-	 */
+/**@author Liam Marshall */
+public class Point implements Comparable<Point> {
+	public Vector3f xyz;
+	public Vector3f rgb;
+	
+	public Point(double x, double y, double z, double r, double g, double b) {
+		xyz = new Vector3f();
+		rgb = new Vector3f();
+		xyz.x = (float) x;
+		xyz.y = (float) y;
+		xyz.z = (float) z;
+		rgb.x = (float) r;
+		rgb.y = (float) g;
+		rgb.z = (float) b;
+	}
+	
+	public void setX(double x) {
+		xyz.x = (float) x;
+	}
+	public void setY(double y) {
+		xyz.y = (float) y;
+	}
+	public void setZ(double z) {
+		xyz.z = (float) z;
+	}
+	public void setR(double r) {
+		rgb.x = (float) r;
+	}
+	public void setG(double g) {
+		rgb.y = (float) g;
+	}
+	public void setB(double b) {
+		rgb.z = (float) b;
+	}
+	
+	public float getX() {
+		return xyz.x;
+	}
+	public float getY() {
+		return xyz.y;
+	}
+	public float getZ() {
+		return xyz.z;
+	}
+	public float getR() {
+		return rgb.x;
+	}
+	public float getG() {
+		return rgb.y;
+	}
+	public float getB() {
+		return rgb.z;
+	}
+	public Vector3f getXYZ() {
+		return xyz;
+	}
+	public Vector3f getRGB() {
+		return rgb;
+	}
+	
 	@Override
 	public int compareTo(Point p) {
-		return (int) ((int)p.x - (int)this.x);
+		return (int)(((this.xyz.y - p.getY() ) * 2) + (this.xyz.x - p.getX())) * 100;
 	}
 }
---
+++ src/mainMenu.java
@@ -0,0 +1,195 @@
+import static org.lwjgl.opengl.GL11.GL_AMBIENT;
+import static org.lwjgl.opengl.GL11.GL_COLOR_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DEPTH_BUFFER_BIT;
+import static org.lwjgl.opengl.GL11.GL_DIFFUSE;
+import static org.lwjgl.opengl.GL11.GL_FILL;
+import static org.lwjgl.opengl.GL11.GL_FRONT;
+import static org.lwjgl.opengl.GL11.GL_FRONT_AND_BACK;
+import static org.lwjgl.opengl.GL11.GL_SHININESS;
+import static org.lwjgl.opengl.GL11.GL_TRIANGLES;
+import static org.lwjgl.opengl.GL11.glBegin;
+import static org.lwjgl.opengl.GL11.glClear;
+import static org.lwjgl.opengl.GL11.glColor3d;
+import static org.lwjgl.opengl.GL11.glEnd;
+import static org.lwjgl.opengl.GL11.glGenLists;
+import static org.lwjgl.opengl.GL11.glLoadIdentity;
+import static org.lwjgl.opengl.GL11.glMaterial;
+import static org.lwjgl.opengl.GL11.glMaterialf;
+import static org.lwjgl.opengl.GL11.glNormal3f;
+import static org.lwjgl.opengl.GL11.glPolygonMode;
+import static org.lwjgl.opengl.GL11.glTexCoord2f;
+import static org.lwjgl.opengl.GL11.glVertex3f;
+
+import java.awt.Canvas;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
+import javax.swing.JFrame;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.input.Mouse;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GL11;
+import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector3f;
+
+/**@author Keegan Bruer */
+public class mainMenu {
+
+	public JFrame frmRainbowCaddy;
+	public static mainMenu window;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		new Thread(new controlFrame()).start();
+				try {
+					mainMenu window = new mainMenu();
+					mainMenu.window = window;
+					window.frmRainbowCaddy.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+	}
+	/**
+	 * Create the application.
+	 */
+	public mainMenu() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	public static boolean frameOpen = true;
+	private void initialize() {
+		frmRainbowCaddy = new JFrame();
+		frmRainbowCaddy.setBounds(100, 100, 634, 474);
+		frmRainbowCaddy.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmRainbowCaddy.getContentPane().setLayout(null);
+	
+		Canvas canvas = new Canvas();
+		canvas.setSize(frmRainbowCaddy.getWidth(), frmRainbowCaddy.getHeight());	
+		
+		frmRainbowCaddy.getContentPane().add(canvas);
+		frmRainbowCaddy.setUndecorated(true);
+		frmRainbowCaddy.setVisible(true);
+		
+		try {
+			Display.setParent(canvas);
+			Display.create();
+		} catch (LWJGLException e) {
+			e.printStackTrace();
+			 System.err.println("The display wasn't initialized correctly. :(");
+	         Display.destroy();
+	         System.exit(1);
+		}
+		
+		setUpDisplayLists();
+        setUpCamera();
+
+       while (frameOpen) {
+            render();
+            checkInput();
+            Display.update();
+            Display.sync(60);
+        }
+        cleanUp();
+	}
+    private static Camera camera;
+    public static Model m = null;
+
+	private static void setUpDisplayLists() {
+	        glGenLists(1);
+	        GL11.glShadeModel(GL11.GL_SMOOTH);
+	        try {
+	          //m = OBJLoader.loadTexturedModel(new File("res/model/model.obj"));
+	            m = OBJLoader.loadModel(new File("res/model/model.obj"));
+	            System.out.println("Model Loaded");
+	        } catch (FileNotFoundException e) {
+	            e.printStackTrace();
+	            Display.destroy();
+	            System.exit(1);
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	            Display.destroy();
+	            System.exit(1);
+	        }
+	    }
+	
+	private static void checkInput() {
+      //camera.processMouse(1, 80, -80);
+        camera.processKeyboard(16, 10, 10, 10);
+        if (Mouse.isButtonDown(0)) {
+            Mouse.setGrabbed(true);
+        } else if (Mouse.isButtonDown(1)) {
+            Mouse.setGrabbed(false);
+        }
+    }
+
+    private static void cleanUp() {
+        try {
+        	Display.destroy();
+        } catch (Exception e) {
+        	
+        }
+        ArrayList<Point> array = new ArrayList<Point>();
+        Collections.sort(array);
+    }
+
+    private static void render() {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glLoadIdentity();
+        camera.applyTranslations();
+        glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+        
+        glBegin(GL_TRIANGLES);
+        for (Model.Face face : m.getFaces()) {
+        	//glColor3d(0,0,1); //green
+            Vector3f n1 = m.getNormals().get(face.getNormalIndices()[0] - 1);
+            glNormal3f(n1.x, n1.y, n1.z);
+            if (m.hasTextureCoordinates()) {
+            	Vector2f t1 = m.getTextureCoordinates().get(face.getTextureCoordinateIndices()[0] - 1);
+            	GL11.glTexCoord2f(t1.x, t1.y);
+            }
+            Vector3f v1 = m.getVertices().get(face.getVertexIndices()[0] - 1);
+            glVertex3f(v1.x, v1.y, v1.z);
+
+        	//glColor3d(0,1,0); //green
+            Vector3f n2 = m.getNormals().get(face.getNormalIndices()[1] - 1);
+            glNormal3f(n2.x, n2.y, n2.z);
+            if (m.hasTextureCoordinates()) {
+            	Vector2f t2 = m.getTextureCoordinates().get(face.getTextureCoordinateIndices()[1] - 1);
+            	GL11.glTexCoord2f(t2.x, t2.y);
+            }
+            Vector3f v2 = m.getVertices().get(face.getVertexIndices()[1] - 1);
+            glVertex3f(v2.x, v2.y, v2.z);
+
+        	//glColor3d(1,0,0); //green
+            Vector3f n3 = m.getNormals().get(face.getNormalIndices()[2] - 1);
+            glNormal3f(n3.x, n3.y, n3.z);
+            if (m.hasTextureCoordinates()) {
+            	Vector2f t3 = m.getTextureCoordinates().get(face.getTextureCoordinateIndices()[2] - 1);
+            	GL11.glTexCoord2f(t3.x, t3.y);
+            }
+            Vector3f v3 = m.getVertices().get(face.getVertexIndices()[2] - 1);
+            glVertex3f(v3.x, v3.y, v3.z);
+        }
+        glEnd();
+       
+    }
+    
+    private static void setUpCamera() {
+    	//----------- SIDE - ABOVE -----------
+//        camera = new EulerCamera.Builder().setAspectRatio((float) Display.getWidth() / Display.getHeight())
+//                .setRotation(40f, 65f, 0f).setPosition(-30f, 30f, 30f).setFieldOfView(60).build();
+        //----------- AREIAL -----------------
+        camera = new EulerCamera.Builder().setAspectRatio((float) Display.getWidth() / Display.getHeight())
+                .setRotation(90f, 61f, 0f).setPosition(5f, 40f, 15f).setFieldOfView(60).build();
+        camera.applyOptimalStates();
+        camera.applyPerspectiveMatrix();
+    }
+}
